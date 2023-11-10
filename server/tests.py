1c5d1cc1bad4118434ebbb9f70e34fb9c015862d
loganwyas <loganw9999@gmail.com>
2023-11-10 11:12:42 -0600

    Stop testing

--- server/tests.py
+++ server/tests.py
@@ -3,7 +3,6 @@ from database import Database
 def test_login():
     db = Database()
     user = db.login("user", "password")
-    print(db.get_catalog(""))
     if user:
         assert user[2] == "user"
     else:
