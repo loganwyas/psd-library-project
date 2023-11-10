6aa0ff1020ee7e39878235467b6f5d5b45b2a6f0
loganwyas <loganw9999@gmail.com>
2023-11-10 10:34:12 -0600

    Try debugs

--- server/tests.py
+++ server/tests.py
@@ -3,6 +3,7 @@ from database import Database
 def test_login():
     db = Database()
     user = db.login("user", "password")
+    print(db.get_catalog(""))
     if user:
         assert user[2] == "user"
     else:
