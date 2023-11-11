e8aecf18e7cbb151c1579770c2d134db3c030e83
Param Sangani <115575003+paramsangani@users.noreply.github.com>
2023-11-10 22:12:50 -0600

    Update tests.py with item hold
    added another test case for get item from id and then added 2 test cases for item on hold. Need to check if they fail

--- server/tests.py
+++ server/tests.py
@@ -1,6 +1,7 @@
 from database import Database
 import string
 import random
+import time
 
 def getRandomString():
     return ''.join(random.choices(string.ascii_uppercase +
@@ -66,6 +67,61 @@ def test_failed_get_library_from_user():
     db = Database()
     library = db.get_library_from_user(7838941)
     assert not library
+
+def test_get_item_from_id():
+    db = Database()
+    item_id = 1  # Replace with the actual item ID in your database
+    expected_author = "J.K. Rowling"  # Replace with the actual expected author value
+    expected_release = 1997  # Replace with the actual expected release year
+
+    item = db.get_item_from_id(item_id)
+    assert (item and item["author"] == expected_author and item["release"] == expected_release)
+
+def test_put_item_hold_by_librarian():
+    db = Database()
+    
+    # Assuming you have a librarian user and a library in your database
+    librarian_user_id = 1  # Replace with the actual librarian user ID
+    library_id = 1  # Replace with the actual library ID
+    item_id = 1  # Replace with the actual item ID
+    
+    # Use a unique status and date for each test to avoid conflicts
+    status = getRandomString()
+    date = int(time.time())  # Current timestamp
+    
+    success = db.put_item_hold(librarian_user_id, library_id, item_id, status, date)
+    assert success
+
+def test_put_item_hold_without_librarian_privileges():
+    db = Database()
+    
+    # Assuming you have a regular user and a library in your database
+    user_id = 2  # Replace with the actual regular user ID
+    library_id = 1  # Replace with the actual library ID
+    item_id = 1  # Replace with the actual item ID
+    
+    # Use a unique status and date for each test to avoid conflicts
+    status = getRandomString()
+    date = int(time.time())  # Current timestamp
+    
+    success = db.put_item_hold(regular_user_id, library_id, item_id, status, date)
+    assert not success
+
+def test_put_item_hold_in_nonexistent_library():
+    db = Database()
+    
+    # Assuming you have a user and a non-existent library ID
+    user_id = 3  # Replace with the actual user ID
+    nonexistent_library_id = 9999  # Replace with a non-existent library ID
+    item_id = 1  # Replace with the actual item ID
+    
+    # Use a unique status and date for each test to avoid conflicts
+    status = getRandomString()
+    date = int(time.time())  # Current timestamp
+    
+    success = db.put_item_hold(user_id, nonexistent_library_id, item_id, status, date)
+    assert not success
+
         
 if __name__ == '__main__':
     db = Database()
